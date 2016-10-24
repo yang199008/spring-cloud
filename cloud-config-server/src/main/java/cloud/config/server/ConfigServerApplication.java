55238e34fc2483a7f0088358ae534ccb5e81b7cd
杨洋 <yang19900809@163.com>
2016-10-24 17:27:18 +0800

    1

--- cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
+++ cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
@@ -6,14 +6,16 @@ package cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableDiscoveryClient
+//@Configuration
+//@EnableAutoConfiguration
+//@EnableDiscoveryClient
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
